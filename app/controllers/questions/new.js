2826f11384f9dc7695b2fe282ecd166306b6acd8
chase <chase.reckling@gmail.com>
2015-06-19 12:46:57 -0700

    add title to questions, style question page.

--- app/controllers/questions/new.js
+++ app/controllers/questions/new.js
@@ -5,12 +5,14 @@ export default Ember.Controller.extend({
     addQuestion: function() {
       var newQuestion = this.store.createRecord('question', {
         name: this.get('name'),
+        title: this.get('title'),
         body: this.get('body'),
         date: this.get('date')
       });
       newQuestion.save();
       this.setProperties({
         name: '',
+        title: '',
         body: '',
         date: ''
               });
--- app/models/question.js
+++ app/models/question.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  title: DS.attr('string'),
   body: DS.attr('string'),
   date: DS.attr('string')
 });
